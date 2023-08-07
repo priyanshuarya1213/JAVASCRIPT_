a7b49006ac8d015c7e81c3515e8cd764f2289d36
Priyanshu Arya <91243140+priyanshuarya1213@users.noreply.github.com>
2023-08-07 15:43:58 +0530

    Basic JavaScript

---
+++ 01_BAsic/conversionOperation.js
@@ -0,0 +1,26 @@
+let score = "Priyanshu"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+// true =1 , false = 0
+
+let isLoggedIn = 1
+
+let booleanIsLoogedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoogedIn);
+
+// " " = false
+// "Priyanshu" = true
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
