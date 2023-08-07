900a1d879c3dfa7c30a026d35936a46d2df24852
Priyanshu Arya <91243140+priyanshuarya1213@users.noreply.github.com>
2023-08-07 15:27:24 +0530

    javascript starting

---
+++ 01_BAsic/datatype1.js
@@ -0,0 +1,25 @@
+"user strict"; // treat all js code as newer version
+
+// alert("hello") //we are using node js not browser
+
+console.log(3+3)
+console.log("Priyanshu")
+
+
+let name = "Priyanshu"
+let age = 22
+let isLoggedIn = false
+
+// nunber => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
---
+++ 01_BAsic/variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553 
+let accountEmial = "priyanshuarya1@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2
+
+accountEmial = "hc@hc.com"
+accountPassword = "1233"
+accountCity = "Banglore"
+
+console.log(accountId);
+
+console.table([accountId, accountEmial, accountPassword, accountCity, accountState])
