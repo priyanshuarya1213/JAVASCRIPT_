e01dd66df1d705f07abfe42b12cf71862de40293
Priyanshu Arya <91243140+priyanshuarya1213@users.noreply.github.com>
2023-08-07 16:05:35 +0530

    Basic JavaScript

--- 01_BAsic/conversionOperation.js
+++ 01_BAsic/conversionOperation.js
@@ -1,3 +1,4 @@
+/*
 let score = "Priyanshu"
 
 console.log(typeof score);
@@ -24,3 +25,31 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+*/
+
+// *****************Operations***********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+
+let str1 = "Hello"
+let str2 = " Priyanshu"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+console.log(true);
